4555e1d4f1316b9ee6257a236bee7c4a48620d8b
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2016-03-31 01:28:44 +0200

    Added the automated acception of the mscorefonts eula to the script

--- final_installation/terminal_server/04_install_xubuntu-desktop.sh
+++ final_installation/terminal_server/04_install_xubuntu-desktop.sh
@@ -4,6 +4,18 @@
 # install the xubuntu-desktop and some additionally packages
 #
 
+#
+# Part of the installation are the ttf-mscorefonts installation,
+# therefore we need to accept a licence. This is automated following
+# the instruction at:
+#                      http://askubuntu.com/questions/16225/how-can-i-accept-the-microsoft-eula-agreement-for-ttf-mscorefonts-installer
+#
+
+echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | \
+    sudo debconf-set-selections
+echo ttf-mscorefonts-installer msttcorefonts/present-mscorefonts-eula note | \
+    sudo debconf-set-selections
+
 sudo apt-get install --assume-yes \
     xubuntu-artwork \
     xubuntu-community-wallpapers \
