9b974d3c8a57e62f167d53010614bc67d32f6bdc
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2016-03-30 17:20:09 +0200

    Added first script with message

---
+++ final_installation/terminal_server/00_install_plain_ubuntu_server_14.04.1_and_OpenSSH_server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat<<EOF
+
+******************************************
+* First step                             *
+******************************************
+
+First we have to install a plan Ubuntu 14.04.1 LTS server
+
+besides the default user genomics I need to install OpenSSH-server to enable access via SSH
+
+
+EOF
