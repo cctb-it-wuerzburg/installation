484fc1cef37849a8874aa0001c057ee96c3d1fe9
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2016-03-30 18:21:30 +0200

    Added installation of xubuntu desktop

---
+++ final_installation/terminal_server/04_install_xubuntu-desktop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#
+# install the xubuntu-desktop and some additionally packages
+#
+
+sudo apt-get install --assume-yes \
+    xubuntu-artwork \
+    xubuntu-community-wallpapers \
+    xubuntu-default-settings \
+    xubuntu-desktop \
+    xubuntu-docs \
+    xubuntu-icon-theme \
+    xubuntu-restricted-addons \
+    xubuntu-restricted-extras \
+    xubuntu-wallpapers
