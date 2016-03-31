6b07989c0ae6a281df83992dfabc5b0e5ce8012b
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2016-03-31 17:26:58 +0200

    Added monitoring via snmp of HP servers

---
+++ final_installation/terminal_server/92_install_hp_snmp_monitoring.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+## Add HP Management Component Pack Mirror
+echo "deb http://downloads.linux.hpe.com/SDR/downloads/MCP/ubuntu trusty/current non-free" | sudo tee /etc/apt/sources.list.d/hpmcp.sources.list
+
+wget -O - 'http://downloads.linux.hpe.com/SDR/repo/mcp/GPG-KEY-mcp' | sudo apt-key add -
+
+sudo apt-get update
+
+sudo apt-get install --assume-yes \
+    hp-health \
+    hp-snmp-agents
+
+# for safety reason
+sudo mkdir -p /etc/snmp
+
+if [ -e /etc/snmp/snmpd.conf ]
+then
+    mv /etc/snmp/snmpd.conf /etc/snmp/snmpd.conf.old
+fi
+
+echo "dlmod cmaX /usr/lib/libcmaX64.so
+rwcommunity private 127.0.0.1/32
+rocommunity public 127.0.0.1/32
+rwcommunity private 132.187.22.169/32
+rocommunity public 132.187.22.169/32
+trapcommunity private
+trapsink 132.187.22.169/32 private" | sudo tee /etc/snmp/snmpd.conf
+
+for i in hp-health hp-snmp-agents snmpd
+do
+    sudo service "$i" restart
+done
