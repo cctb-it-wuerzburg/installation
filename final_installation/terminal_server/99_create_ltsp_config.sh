edc2f92ad101546c2bd1fe73e2e9f17a5922a10f
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2016-04-02 00:30:28 +0200

    Support for multiple architectures added
    Instead of only preparing the LTSP image for amd64 thin clients, i386
    clients are now supported as well.
    
    Fix #2

--- final_installation/terminal_server/99_create_ltsp_config.sh
+++ final_installation/terminal_server/99_create_ltsp_config.sh
@@ -9,32 +9,34 @@
 # get the current IP address
 IP_ADDRESS=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
 
-sudo ltsp-build-client --arch amd64
-sudo sed -i 's/ipappend 2/ipappend 3/g' /var/lib/tftpboot/ltsp/amd64/pxelinux.cfg/default
+# create a set for the architectures: amd64, i386
+for ltsp_arch in i386 amd64
+do
+    sudo ltsp-build-client --arch ${ltsp_arch}
+    sudo sed -i 's/ipappend 2/ipappend 3/g' /var/lib/tftpboot/ltsp/${ltsp_arch}/pxelinux.cfg/default
 
-sudo chroot /opt/ltsp/amd64 apt-get update
-sudo chroot /opt/ltsp/amd64 apt-get upgrade
+    sudo chroot /opt/ltsp/${ltsp_arch} apt-get update
+    sudo chroot /opt/ltsp/${ltsp_arch} apt-get upgrade
 
-# add a genomics user to the images
-sudo chroot /opt/ltsp/amd64 mkdir -p /home2
-sudo chroot /opt/ltsp/amd64 useradd --comment "Genomics user" --home-dir /home2/genomics --create-home --shell /bin/bash --password '$6$wFb.dfXL3p5$MRmV2SA49DW/XIZJotdZuQ2rfeHy5/BPy4/x257pT1HR8sfz1VadxOKIjbtTqsfwUjbZhxV1b7YAKgv2ToYsA0' --groups sudo genomics
+    # add a genomics user to the images
+    sudo chroot /opt/ltsp/${ltsp_arch} mkdir -p /home2
+    sudo chroot /opt/ltsp/${ltsp_arch} useradd --comment "Genomics user" --home-dir /home2/genomics --create-home --shell /bin/bash --password '$6$wFb.dfXL3p5$MRmV2SA49DW/XIZJotdZuQ2rfeHy5/BPy4/x257pT1HR8sfz1VadxOKIjbtTqsfwUjbZhxV1b7YAKgv2ToYsA0' --groups sudo genomics
 
-# install openssh-server to allow login into the nodes
-# (taken from https://help.ubuntu.com/community/UbuntuLTSP/ClientTroubleshooting)
-sudo mount --bind /dev /opt/ltsp/amd64/dev
-sudo mount -t proc none /opt/ltsp/amd64/proc
-LTSP_HANDLE_DAEMONS=false sudo chroot /opt/ltsp/amd64 apt-get install --assume-yes openssh-server
+    # install openssh-server to allow login into the nodes
+    # (taken from https://help.ubuntu.com/community/UbuntuLTSP/ClientTroubleshooting)
+    sudo mount --bind /dev /opt/ltsp/${ltsp_arch}/dev
+    sudo mount -t proc none /opt/ltsp/${ltsp_arch}/proc
+    LTSP_HANDLE_DAEMONS=false sudo chroot /opt/ltsp/${ltsp_arch} apt-get install --assume-yes openssh-server
 
-# remove the ssh_host_*_keys from exclude list
-sudo sed -i '/^etc\/ssh\/ssh_host_*_key/d' /etc/ltsp/ltsp-update-image.excludes
+    # remove the ssh_host_*_keys from exclude list
+    sudo sed -i '/^etc\/ssh\/ssh_host_*_key/d' /etc/ltsp/ltsp-update-image.excludes
 
-# Deinstall lightlocker and lightlocker
-# this caused problems with screen locking
-sudo chroot /opt/ltsp/amd64 apt-get purge --assume-yes light-locker light-locker-settings
+    # Deinstall lightlocker and lightlocker
+    # this caused problems with screen locking
+    sudo chroot /opt/ltsp/${ltsp_arch} apt-get purge --assume-yes light-locker light-locker-settings
 
-# Set a lts.conf file to enable the correct SERVER and DHCP on the clients
-cat <<EOF | sudo tee /var/lib/tftpboot/ltsp/amd64/lts.conf
-[Default]
+    # Set a lts.conf file to enable the correct SERVER and DHCP on the clients
+    echo "[Default]
 # For troubleshooting, the following open a local console with Alt+Ctrl+F2.
 SCREEN_02=shell
 SCREEN_07=ldm
@@ -45,22 +47,27 @@ NETWORK_COMPRESSION=True
 
 # this is the important line to enable DHCP on the client machines
 NET_DEVICE_METHOD=dhcp
+" | sudo tee /var/lib/tftpboot/ltsp/${ltsp_arch}/lts.conf
 
-EOF
+    # move the created lts.conf to /opt/ltsp/${ltsp_arch}/etc/lts.conf
+    if [ -e /opt/ltsp/${ltsp_arch}/etc/lts.conf ]
+    then
+	sudo mv /opt/ltsp/${ltsp_arch}/etc/lts.conf /opt/ltsp/${ltsp_arch}/etc/lts.conf.old
+    fi
+    sudo cp /var/lib/tftpboot/ltsp/${ltsp_arch}/lts.conf /opt/ltsp/${ltsp_arch}/etc/lts.conf
 
-# move the created lts.conf to /opt/ltsp/amd64/etc/lts.conf
-if [ -e /opt/ltsp/amd64/etc/lts.conf ]
-then
-  sudo mv /opt/ltsp/amd64/etc/lts.conf /opt/ltsp/amd64/etc/lts.conf.old
-fi
-sudo cp /var/lib/tftpboot/ltsp/amd64/lts.conf /opt/ltsp/amd64/etc/lts.conf
+    # install check_mk into the clients
+    sudo chroot /opt/ltsp/${ltsp_arch} apt-get --assume-yes install xinetd check-mk-agent check-mk-agent-logwatch
+    sudo chroot /opt/ltsp/${ltsp_arch} cp /etc/xinetd.d/check_mk /etc/xinetd.d/check_mk.bak
+    sudo chroot /opt/ltsp/${ltsp_arch} sed -i '/disable/s/^.*$/\tdisable        = no/' /etc/xinetd.d/check_mk
+    sudo chroot /opt/ltsp/${ltsp_arch} sed -i '/only_from/s/^.*$/\tonly_from = 132.187.22.169/' /etc/xinetd.d/check_mk
 
-# install check_mk into the clients
-sudo chroot /opt/ltsp/amd64 apt-get --assume-yes install xinetd check-mk-agent check-mk-agent-logwatch
-sudo chroot /opt/ltsp/amd64 cp /etc/xinetd.d/check_mk /etc/xinetd.d/check_mk.bak
-sudo chroot /opt/ltsp/amd64 sed -i '/disable/s/^.*$/\tdisable        = no/' /etc/xinetd.d/check_mk
-sudo chroot /opt/ltsp/amd64 sed -i '/only_from/s/^.*$/\tonly_from = 132.187.22.169/' /etc/xinetd.d/check_mk
+    # umount the mounts
+    sudo umount /opt/ltsp/${ltsp_arch}/dev
+    sudo umount /opt/ltsp/${ltsp_arch}/proc
+
+done
 
 # update ltsp-image
 sudo ltsp-update-image
-sudo sed -i 's/ipappend 2/ipappend 3/g' /var/lib/tftpboot/ltsp/amd64/pxelinux.cfg/default
+find /var/lib/tftpboot/ltsp/ -name default | sudo xargs sed -i 's/ipappend 2/ipappend 3/g'
