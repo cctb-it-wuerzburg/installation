0dad2b63edaea4102779f89215037b0ffb7d6ff1
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2016-04-04 17:00:14 +0200

    Assume yes on the installation

--- final_installation/storage_server/20_install_zfs_packages.sh
+++ final_installation/storage_server/20_install_zfs_packages.sh
@@ -4,11 +4,11 @@ sudo apt-get --assume-yes install software-properties-gtk
 
 # install ZFS repository
 
-sudo add-apt-repository ppa:zfs-native/stable
+sudo add-apt-repository --yes ppa:zfs-native/stable
 
 sudo apt-get update
 
 # install ZFS package
 
-sudo apt-get install ubuntu-zfs
+sudo apt-get --assume-yes install ubuntu-zfs
 
