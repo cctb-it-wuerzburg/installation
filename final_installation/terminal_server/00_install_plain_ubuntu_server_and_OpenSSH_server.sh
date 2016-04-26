8a42bef75f1252442a9011be7568bd254fb76aae
Frank Förster <foersterfrank@gmx.de>
2016-04-26 13:17:11 +0200

    Deleted Ubuntu version from file name
    I deleted the Ubuntu version number from the filename and updated the text to point to https://github.com/greatfireball/create-ubuntu-autoinstall-medium for an automated installation of Ubuntu.
    
    Fixes #10

--- final_installation/terminal_server/00_install_plain_ubuntu_server_and_OpenSSH_server.sh
+++ final_installation/terminal_server/00_install_plain_ubuntu_server_and_OpenSSH_server.sh
@@ -6,9 +6,11 @@ cat<<EOF
 * First step                             *
 ******************************************
 
-First we have to install a plan Ubuntu 14.04.1 LTS server
+First we have to install a plan Ubuntu LTS server
 
 besides the default user genomics I need to install OpenSSH-server to enable access via SSH
+
+This can be done by using the scripts provided by https://github.com/greatfireball/create-ubuntu-autoinstall-medium
 
 
 EOF
