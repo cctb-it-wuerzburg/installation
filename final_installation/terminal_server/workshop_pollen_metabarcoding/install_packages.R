531f86d42a0ea78d4a5ae60ae8c008cf23b92b06
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2016-05-05 00:42:34 +0200

    Added packages which are required by Alex code

--- final_installation/terminal_server/workshop_pollen_metabarcoding/install_packages.R
+++ final_installation/terminal_server/workshop_pollen_metabarcoding/install_packages.R
@@ -1,3 +1,7 @@
+repo="https://cloud.r-project.org/"
+install.packages("bipartite", repos=repo)
+install.packages("", repos=repo);
+
 source("http://bioconductor.org/biocLite.R")
 biocLite("phyloseq")
 
