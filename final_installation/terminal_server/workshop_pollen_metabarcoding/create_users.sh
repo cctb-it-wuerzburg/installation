9ea4941e4f7fdf7a9f44e1217a5dbe653283cfae
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2016-05-04 13:28:43 +0200

    Added script to create users for workshop

---
+++ final_installation/terminal_server/workshop_pollen_metabarcoding/create_users.sh
@@ -0,0 +1,33 @@
+groupadd students
+useradd --comment 'Michael Fizdale' --password '$6$IVXJlR4DgJ8z7$uGjVJ9s7r.69iv.KQu400JwzGP4PC19y1LMXioEMHG6Qj8onSy20WBiB680GWORhZWOeuHmUkAfNiq3UZo/v70' --home-dir /home/micfiz --create-home --shell /bin/bash -g students micfiz
+useradd --comment 'Rachael Bonoan' --password '$6$xsqS4OXlhtV0LD$X9CnvhjoT.1PB2QocyPKHJeNhzMCQpEVIRegYuVwmwpFXzr/S2pjyN4I7v9Pa3edtIt23yD8ToRfb60216zuu1' --home-dir /home/racbon --create-home --shell /bin/bash -g students racbon
+useradd --comment 'Irena Dzimrevska' --password '$6$D2zhpBV3oGe$giwNgSdfes3xy6Py6nZ2RI7e1v.i7S.oMmMyBMaJVXPIKujixetsgo1gB/tBqGlJSVn/FFdn50G1ycWnSQTP91' --home-dir /home/iredzi --create-home --shell /bin/bash -g students iredzi
+useradd --comment 'Saorla Kavanagh' --password '$6$uDgBqJ9JUcJ$FfSRtjhoHQ.eoO2Nl5fs.YBd/Lh6NuAL8p9BK3LWNgI7ybGac1s2FCSzV97l.htVO8cSFwx0doSJEs0Kyldqd0' --home-dir /home/saokav --create-home --shell /bin/bash -g students saokav
+useradd --comment 'Melisa Geisa' --password '$6$yGT3Le9K.u8FE$CoWWntxlp4XTExhdX4ZA0Ly1lHIYI5rkOKHq4dtPJupI4ZHkBaQ0fHyD2yFTjzE272uE.wsHGWwM01B31p0DW/' --home-dir /home/melgei --create-home --shell /bin/bash -g students melgei
+useradd --comment 'Rodney T. Richardson' --password '$6$glsMxRSpOjF$CVcWFYn0S5aAazhW5cahNjKkRc4Zq.94Pl/ghBFZWGz3vTu5aHMrotM2ELsAEpl6rMtGfq6zdx7qncS9Xv8gr.' --home-dir /home/rodric --create-home --shell /bin/bash -g students rodric
+useradd --comment 'Lourdes Boero' --password '$6$vGoGx7JMl$GpF4nMMprfKOnS7T.DGpefPmBEjRK585ySnjrSbhVmRMF8/UYP/al6qqqtteNUPzCHiCZgYsY4TPK8NC9f4Qf0' --home-dir /home/louboe --create-home --shell /bin/bash -g students louboe
+useradd --comment 'Marcin Grabowski' --password '$6$L7sGbfMgJi70/ks1$q5mZ/BZnyNL2JMaHzly7zixoq95VgLFeDEm8jzY0GuxQr7cvInjWE3eD9hkKU5yHPsn1hYyJKSMpBim0STPn0/' --home-dir /home/margra --create-home --shell /bin/bash -g students margra
+useradd --comment 'Laura Jones' --password '$6$DDreyDXI5w$H1viGJ9USwjVwsBvIH/XkVp2zOBEU.YDMlacnbm.xA.x5Dy808yKlLfa8.3jwlOQNs4omUCLq2Cug0.tXGt/S1' --home-dir /home/laujon --create-home --shell /bin/bash -g students laujon
+useradd --comment 'Anthony Vaudo' --password '$6$S1ku5M2VK9t$ObPwwD2LPYfrAYD0JjZZjk9ZKFi9NEOt3J1qYkgJLPI7rQsc.KG6IMmxErMJ1.IrIHmprtzsdWZ.VjTc/2BH//' --home-dir /home/antvau --create-home --shell /bin/bash -g students antvau
+useradd --comment 'Georgina Brennan' --password '$6$pGxk720rT$ixOY4IeTP5QP8vxSUOSg3FszFkNaIfxfPtgCggr3rIzgez1YqqyS0SitGK9rDqvu1WTYjpLxtd/GkAbsCuVQ//' --home-dir /home/geobre --create-home --shell /bin/bash -g students geobre
+useradd --comment 'Blaz Podriznik' --password '$6$m7OzkV3/$3t6U8oSrDqP8ivrkuYTzphDHaItO5dHjsAmCpoP/okS0XieQ5mOL0LypZ0WEENgr0g1BQUyTWyQC0AOj9U1Cr0' --home-dir /home/blapod --create-home --shell /bin/bash -g students blapod
+useradd --comment 'Vicente Martinez Lopez' --password '$6$x/nOFrFZ0Si00$6Cg6ZcOqa2dUPievPNwvpAGTnPsqaG92JinmRF6OcLGzxj.owvy6NOQya1ZqMPaKknwmEXWLiv/DuEWDz5OQG1' --home-dir /home/viclop --create-home --shell /bin/bash -g students viclop
+useradd --comment 'Annemarie Gous' --password '$6$.OzckMt0L4E/mXVZ$nKMvq6hEsZmvyBkc2JEL10NTJC7E2/SXyyQSWfpLKbaO81EFY7Fm/At4xgTubQI9pCMY7mJhE3TFBRy90krRd1' --home-dir /home/anngou --create-home --shell /bin/bash -g students anngou
+useradd --comment 'Gerit Dreyersdorff' --password '$6$oKMB0a80bXE.LT$hik0rWBDiUjcMgatnycbLaH0kSiuT3mIQ/0i5zplq2Nf6BsvFbFZu3qAwBmFx6CjjFHv6uKvwMoeVGtkT.ZMK1' --home-dir /home/gerdre --create-home --shell /bin/bash -g students gerdre
+useradd --comment 'S_D. PRADEEPA' --password '$6$zZp4kObrqgSf7$hHSwOEZ7pyrCQC6tRPWMLzBDyd7Z47VBEJTAzVNMjEPyH8P4GIi26MqolcmLXfV15xKLcG.Uk3Wn.T7JwLXU51' --home-dir /home/s_dpra --create-home --shell /bin/bash -g students s_dpra
+useradd --comment 'Lynn Dicks' --password '$6$4zoqez57KlS9XSU$qoKFWpDMs.r1sW9cKTMWZHBOfzNzSAL745AIIk5dc8jN1sMqCNA1Mg/wvuxBEbqpAUdx5swWoZrXYqMcErJJo.' --home-dir /home/lyndic --create-home --shell /bin/bash -g students lyndic
+useradd --comment 'Tyler Smith' --password '$6$B.gj/NXS$g58krzHJEpaQI75rp9rBP6hbeNXavYrSmesjR3f0YYAug2PYNIO2n8hfAyo8OiyzndPZEhqnAK1ntGyNl3sih1' --home-dir /home/tylsmi --create-home --shell /bin/bash -g students tylsmi
+useradd --comment 'Song Wang' --password '$6$YSjTLmK34qWnY$aAOfQNiBxKKZNW5ZcOFdlodGj3ty4NA0coeUf3adJp3JTOkrWiSPCwU7ozqHJf.xX9CMRGTkDeYWokt8U1oZb0' --home-dir /home/sonwan --create-home --shell /bin/bash -g students sonwan
+useradd --comment 'Riikka Kaartinen' --password '$6$rj0J0n6KKoQ$jepmFCdGgSngCEzYari.XgtFPwqAa4vha4ATRZnUjWKAd9nfx8ya1jikZ.ZH6xnQZN379qdq.FCIUBQFh6oCU0' --home-dir /home/riikaa --create-home --shell /bin/bash -g students riikaa
+useradd --comment 'Birgit Gemeinholzer' --password '$6$uQsdNgo/vv0b$jX2kAOdYwwPVLVoQ/XIBZt5xNC7997lEX5Hp8g1AjcZqVlN2sGi4P0RnO8XMNQ8wV3fW0o.MdqjJoBqoFY/PH/' --home-dir /home/birgem --create-home --shell /bin/bash -g students birgem
+useradd --comment 'Stephanie Swenson' --password '$6$qE49BENf7O4UxJI$6B0DlheToN3brzh4pcOeaGKKOqRMy6k7i.NmadzFHcOvkBWS40Hg1mmqQUckOVmD4Kd1DsIyQ6ZR7xLem0ldk/' --home-dir /home/steswe --create-home --shell /bin/bash -g students steswe
+useradd --comment 'Natasha de Vere' --password '$6$is47jsjc6W$xqBU3RLVTdrgxTRY9oRGLI0gHkqA2DoBbCcSDIilaIQVfgCxzDiTssgBvKlV4uXe.Cx/VX1qNw.6emgd1oEk..' --home-dir /home/natver --create-home --shell /bin/bash -g students natver
+useradd --comment 'Markus Ankenbrand' --password '$6$B8J1ziLhQc$dUVQPtTBIzlbaSNCS6qK54h8DzRN1.9zC8mVoJsd87VdiuTw6ZDRcfLTZDxM61PUeCudT8hfSe41aOz9Qn5hg.' --home-dir /home/marank --create-home --shell /bin/bash -g students marank
+useradd --comment 'Alexander Keller' --password '$6$29sRgai/gb$tA5O9cq3AGraze42W.bzw1zvTVq6LhSZnn7wCKI.lf2H/2W5vuJ7cjA0wAiaCFeDTMN1sGTsjTVr9NDnw8AfZ.' --home-dir /home/alekel --create-home --shell /bin/bash -g students alekel
+useradd --comment 'Frank Foerster' --password '$6$udJrzj3y$42vStl87GOH1tiIs4wsXr6TD/J2uX92y7B497tVWOBK/mBeT0Tvw.c1HHAn6E7LRp9.YRgKXpWjec5elMVMYC.' --home-dir /home/frafoe --create-home --shell /bin/bash -g students frafoe
+
+for admin in frafoe alekel marank
+do
+    gpasswd -a "$admin" epoptes
+    gpasswd -a "$admin" sudo
+done
