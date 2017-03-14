9d9c734f76840f60c204433e1320695898b5c97c
Frank Förster <foersterfrank@gmx.de>
2017-03-14 17:25:14 +0100

    Added installation of nullmailer incl. default config

---
+++ final_installation/terminal_server/49_install_nullmailer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+### set default values for our university
+
+echo "
+nullmailer    shared/mailname    string    $HOSTNAME.biologie.uni-wuerzburg.de
+nullmailer    nullmailer/defaultdomain    string    uni-wuerzburg.de
+nullmailer    nullmailer/relayhost    string    mailmaster.uni-wuerzburg.de
+nullmailer    nullmailer/adminaddr    string   frank.foerster@uni-wuerzburg.de
+" | sudo debconf-set-selections
+
+sudo apt install --assume-yes nullmailer
+
+# send a test email
