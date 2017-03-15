8f3656e69919ce5530488f2f535591740743abed
Frank Förster <foersterfrank@gmx.de>
2017-03-15 22:52:34 +0100

    Using cctb-it mailinglist for root account mails

--- final_installation/terminal_server/49_install_nullmailer.sh
+++ final_installation/terminal_server/49_install_nullmailer.sh
@@ -6,7 +6,7 @@ echo "
 nullmailer    shared/mailname    string    $HOSTNAME.biologie.uni-wuerzburg.de
 nullmailer    nullmailer/defaultdomain    string    uni-wuerzburg.de
 nullmailer    nullmailer/relayhost    string    mailmaster.uni-wuerzburg.de
-nullmailer    nullmailer/adminaddr    string   frank.foerster@uni-wuerzburg.de
+nullmailer    nullmailer/adminaddr    string   cctb-it@lists.uni-wuerzburg.de
 " | sudo debconf-set-selections
 
 sudo apt install --assume-yes nullmailer
