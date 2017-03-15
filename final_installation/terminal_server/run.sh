6f7ebf34a3f68ab0847664d986adfc46ff82cea9
Frank Förster <foersterfrank@gmx.de>
2017-03-15 19:43:37 +0100

    Writes error msg to separate file

--- final_installation/terminal_server/run.sh
+++ final_installation/terminal_server/run.sh
@@ -3,6 +3,6 @@
 for i in [0-9]*.sh
 do 
     date +"[%Y-%m-%dT%H:%M:%S] Started script '$i'" | tee --append run.log
-    ./"$i" 2>&1 | tee "$i".log
+    ./"$i" 2> "$i".err | tee "$i".log
     date +"[%Y-%m-%dT%H:%M:%S] Finished script '$i'" | tee --append run.log
 done
