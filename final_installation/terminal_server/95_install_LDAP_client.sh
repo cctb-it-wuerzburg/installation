bfe12bd46d63bc8c43aa7d6048a41abf9cd9dc3f
Frank Förster <foersterfrank@gmx.de>
2017-03-15 02:51:03 +0100

    Adds installation of LDAP client

---
+++ final_installation/terminal_server/95_install_LDAP_client.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "
+ldap-auth-config    ldap-auth-config/bindpw    password    
+ldap-auth-config    ldap-auth-config/rootbindpw    password    
+ldap-auth-config    ldap-auth-config/ldapns/ldap_version    select    3
+ldap-auth-config    ldap-auth-config/move-to-debconf    boolean    true
+ldap-auth-config    ldap-auth-config/dblogin    boolean    false
+ldap-auth-config    ldap-auth-config/dbrootlogin    boolean    false
+ldap-auth-config    ldap-auth-config/pam_password    select    crypt
+ldap-auth-config    ldap-auth-config/ldapns/ldap-server    string    132.187.198.16
+ldap-auth-config    ldap-auth-config/ldapns/base-dn    string    dc=cctb,dc=uni-wuerzburg,dc=de
+ldap-auth-config    ldap-auth-config/binddn    string    cn=proxyuser,dc=example,dc=net
+ldap-auth-config    ldap-auth-config/rootbinddn    string    cn=admin,dc=cctb,dc=uni-wuerzburg,dc=de
+ldap-auth-config    ldap-auth-config/override    boolean    true " | sudo debconf-set-selection
+
+sudo apt install --assume-yes ldap-auth-config libpam-ldap nscd rpcbind libnss-ldap
