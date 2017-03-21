78fbbb0acfff93bfd9082cf50367d2ddab588715
Frank Förster <foersterfrank@gmx.de>
2017-03-21 02:29:15 +0100

    Adds two admistrator accounts

---
+++ final_installation/storage_server/02_create_admin_user.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+useradd --shell /bin/bash --create-home --base-dir /home2/ --groups sudo --comment "Patrick Haag, patrick.haag@uni-wuerzburg.de" --password '$6$N0joKNnnqF$mavoUr1aqzTrh9bqMZpqy9syWVxFhodLtpC7FQp9nvsIprC9clpoiQ5IZaQXyX4RTF0o8HDqa/VK8.aG3Tkw/.' rzuw273_admin
+useradd --shell /bin/bash --create-home --base-dir /home2/ --groups sudo --comment "Frank Foerster, frank.foerster@uni-wuerzburg.de" --password '$6$U4eARpZ6$PaERdiP9PJQrj/xROmj5zsMo.bvfUMkHeFXITQpUWtq7n47Zg9RcNB7VQ2FnfIR9fioB/l5dTc9IZG2cWj38m.' frf53jh_admin
