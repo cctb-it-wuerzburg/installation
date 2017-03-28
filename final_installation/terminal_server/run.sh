fbb3b38af9a4c0acc4432b0f3f19c59ffc145e7d
Frank Förster <foersterfrank@gmx.de>
2017-03-29 01:01:10 +0200

    Sort list of installation candidates

--- final_installation/terminal_server/run.sh
+++ final_installation/terminal_server/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in [0-9]*.sh
+for i in $(find -name "[0-9]*.sh" | xargs basename -n1 | sort -n)
 do 
     date +"[%Y-%m-%dT%H:%M:%S] Started script '$i'" | tee --append run.log
     ./"$i" 2> "$i".err | tee "$i".log
