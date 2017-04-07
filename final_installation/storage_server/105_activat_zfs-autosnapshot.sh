38bc5beaba3af727db8b549ea37e7964c3e4e0fd
Frank Förster <foersterfrank@gmx.de>
2017-04-07 03:37:50 +0200

    Corrects link to new PPA

--- final_installation/storage_server/105_activat_zfs-autosnapshot.sh
+++ final_installation/storage_server/105_activat_zfs-autosnapshot.sh
@@ -3,6 +3,6 @@
 sudo apt update
 sudo apt install --assume-yes software-properties-common
 
-sudo add-apt-repository --yes --update ppa:zfs-native/stable
+sudo add-apt-repository --yes --update ppa:bob-ziuchkovski/zfs-auto-snapshot
 
 sudo apt install zfs-auto-snapshot
